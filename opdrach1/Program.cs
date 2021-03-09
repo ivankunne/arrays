048eb38c40a5ac080b3a7bc300c81eb807a419e8
ivankunne <ivankunne@gmail.com>
2021-03-09 11:45:47 +0100

    opdracht 1 gemaakt

--- opdrach1/Program.cs
+++ opdrach1/Program.cs
@@ -6,7 +6,18 @@ namespace opdrach1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] broers = { "Peter", "is", "de", "broer", "van", "Hans" };
+            string[] broer = { "Hans", "is", "de", "broer", "van", "Peter" };
+            foreach (string i in broers)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (string i in broer)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
+
+
