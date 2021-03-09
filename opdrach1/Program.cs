3eef42dde68282f385e9b8fdcf08631dfcfa4940
ivankunne <ivankunne@gmail.com>
2021-03-09 11:23:53 +0100

    Add project files.

---
+++ opdrach1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace opdrach1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
