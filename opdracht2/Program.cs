e77af003180dbdfb9c1bd426fec1fcad643f9345
ivankunne <ivankunne@gmail.com>
2021-03-10 12:00:44 +0100

    opdracht 2

--- opdracht2/Program.cs
+++ opdracht2/Program.cs
@@ -6,7 +6,14 @@ namespace opdracht2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = i * 3 + j;
+            {
+                Console.WriteLine();
+            }
+
         }
     }
 }
